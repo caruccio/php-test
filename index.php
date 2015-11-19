7417a29388b4392edae53e20a798447213759bad
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2015-11-19 15:55:59 -0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+
+<body>
+
+<p>
+    <a href=phpinfo.php>phpinfo</a>
+</p>
+
+<p>
+    <form method=get action=echo.php>
+        <input type=submit value="echo"/> <input type=text name="text"/>
+    </form>
+</p>
+
+<p>
+    <a href=fibonacci.php>fibonacci</a> <input type=text name=number>
+</p>
+
+</body>
+</html>
