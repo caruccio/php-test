7b859ceab820d7fe2c9970e85bded4fadb4bfd29
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2016-03-22 18:20:37 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php header('Content-type: application/json') ?>{
   "status": "ok",
   "message": "This is my API root",
-  "hostname: "<?php echo getenv('HOSTNAME') ?>"
+  "hostname": "<?php echo getenv('HOSTNAME') ?>"
 }
